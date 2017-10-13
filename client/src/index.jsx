e881ad9f56e2eea792625b51f45b1ed43f81f978
Gary <yes.murz@gmail.com>
2017-10-12 17:29:10 -0700

    Naive implementation without cookies

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Rand from '../../helpers/randomizer.js';
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       redBall: 0,
-      blueBall: 0
+      blueBall: 0,
+      displayBall: ''
     }
   }
 
+  componentWillMount() {
+    var blueBallElement = <div>BLUE BALL</div>;
+    var redBallElement = <div>RED BALL</div>;
+    this.setState({
+      displayBall: Rand.randomize() ? blueBallElement : redBallElement
+    });
+  }
+
   render() {
+
+
     return (
       <div>
-        HELLO WORLD
+        Welcome to Blue ball - Red ball!
+        <div className='ball'>
+          {this.state.displayBall}
+        </div>
       </div>
     )
   }
