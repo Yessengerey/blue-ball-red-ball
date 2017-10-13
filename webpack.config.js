068940b74569203e226bf9f6fc5ac799b336bbe7
Gary <yes.murz@gmail.com>
2017-10-12 17:17:42 -0700

    Fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,17 @@
 var path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/public/dist');
 
 module.exports = {
-  entry: './client/src',
+  entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
-    path: path.join(__dirname, 'public/dist')
+    path: DIST_DIR
   },
   module: {
     rules: [
       { test: /\.(js|jsx)$/,
-        include: path.join(__dirname, 'client/src'),
+        include: SRC_DIR,
         exclude: ['node_modules'],
         use: [
           { loader: 'babel-loader',
